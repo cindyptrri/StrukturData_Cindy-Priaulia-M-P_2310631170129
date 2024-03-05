816f97769f0d2400a726ebf0d7cecadf4e61088b
cindyptrri <148054114+cindyptrri@users.noreply.github.com>
2024-03-06 00:02:56 +0700

    Add files via upload

---
+++ coba3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#define SIZE 6
+
+typedef int multiplication[SIZE][SIZE];
+
+using namespace std;
+
+int main (){
+    multiplication A;
+    int* multiplied;
+
+    multiplied = &A[0][0];
+
+    for (int i = 5; i<= 10; i++){
+        for (int j = 5; j <= 10; j++){
+            A[i-5][j-5] = i*j;
+            cout << A[i-5][j-5] << " ";
+        }
+        cout << endl;
+    }
+
+    cout << endl;
+
+    cout << " Alamat Array " << endl;
+    for (int i = 0; i < 6; i++){
+        for (int j = 0; j < 6; j++){
+                cout << " Alamat tersimpan A[" << "][" << j << "] :" << multiplied << endl;
+                multiplied++;
+        }
+    }
+}
+
